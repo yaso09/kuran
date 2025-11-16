7e6e125293787f02576f0861b1399c55af748ee5
yaso09 <yasirator04@gmail.com>
2025-11-16 14:35:49 +0300

    fontlar sw.js'e kaydedildi

--- sw.js
+++ sw.js
@@ -1,2 +1,17 @@
-self.addEventListener('install', e => console.log('pwa installed.'));
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('your-cache-name').then(function(cache) {
+      return cache.addAll([
+        "index.html",
+        "kuran.html",
+        "embed.html",
+        "print.min.js",
+        "assets/fonts/alshohadaa.ttf",
+        "assets/fonts/Aqeeq-Bold.ttf",
+        "assets/fonts/Kitab-Bold.ttf",
+        "assets/fonts/StackSansNotch-Bold.ttf"
+      ])
+    })
+  );
+});
 self.addEventListener('fetch', event => {}); // şimdilik bir şey yapmaya gerek yok
