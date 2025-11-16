29a7e6562d9c55a52bbc70bacd7bc445339bfeae
yaso09 <yasirator04@gmail.com>
2025-11-16 13:33:29 +0300

    PWA deneme

--- sw.js
+++ sw.js
@@ -1,23 +1,2 @@
-const CACHE_NAME = 'kuran-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/print.min.js',
-  '/Aqeeq-Bold.ttf',
-  '/StackSansNotch-Bold.ttf',
-  '/alshohadaa.ttf'
-  // İstersen JSON dosyalarını veya CSS/ikonları da ekle
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', e => console.log('pwa installed.'));
+self.addEventListener('fetch', event => {}); // şimdilik bir şey yapmaya gerek yok
